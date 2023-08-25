a72fad69ee6dec4898fee51d165655c4c8a62743
jyrkidn <jyrkidn@users.noreply.github.com>
2023-08-25 10:41:38 +0000

    Fix styling

--- src/Views/Components/Link.php
+++ src/Views/Components/Link.php
@@ -2,8 +2,8 @@
 
 namespace Codedor\SocialMediaLinks\Views\Components;
 
-use Illuminate\View\Component;
 use Illuminate\Contracts\View\View;
+use Illuminate\View\Component;
 
 class Link extends Component
 {
--- src/Views/Components/Links.php
+++ src/Views/Components/Links.php
@@ -3,9 +3,9 @@
 namespace Codedor\SocialMediaLinks\Views\Components;
 
 use Codedor\MediaLibrary\Models\Attachment;
+use Illuminate\Contracts\View\View;
 use Illuminate\Support\Collection;
 use Illuminate\View\Component;
-use Illuminate\Contracts\View\View;
 
 class Links extends Component
 {
