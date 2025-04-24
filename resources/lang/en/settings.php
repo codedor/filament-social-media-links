29857181a41b5c6cbb9617d22dfcb91f98748b8b
Jyrki De Neve <jyrki.deneve@gmail.com>
2025-04-24 11:57:52 +0300

    Translate Setting tab labels

---
+++ resources/lang/en/settings.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'logo structured data image :locale' => 'Structured data image for :locale',
+    'logo structured data' => 'Logo Structured Data',
+    'social media links' => 'Social Media Links',
+];
--- src/Settings/LogoStructuredData.php
+++ src/Settings/LogoStructuredData.php
@@ -14,6 +14,9 @@ class LogoStructuredData implements SettingsInterface
         return LocaleCollection::map(
             fn (Locale $locale) => AttachmentInput::make('filament-social-media-links.image_structured_data_' . $locale->locale() . '_id')
                 ->label('Structured data image for ' . $locale->locale())
+                ->label(__('filament-social-media-links::settings.logo structured data image :locale', [
+                    'locale' => $locale->locale()
+                ]))
                 ->allowedFormats(config('filament-social-media-links.formatter-formats', []))
         )
             ->toArray();
@@ -22,5 +25,10 @@ class LogoStructuredData implements SettingsInterface
     public static function priority(): int
     {
         return 45;
+    }
+
+    public static function title(): string
+    {
+        return __('filament-social-media-links::settings.logo structured data');
     }
 }
