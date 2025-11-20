6c7acf0dc0c5a180342621314a1d7fa14adea3c3
Jyrki De Neve <jyrki.deneve@gmail.com>
2025-11-20 16:11:18 +0200

    Translate Setting tab labels (#18)
    * Translate Setting tab labels
    
    * Fix styling
    
    * Add Dutch translations
    
    ---------
    
    Co-authored-by: jyrkidn <2447042+jyrkidn@users.noreply.github.com>

---
+++ resources/lang/nl/settings.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'logo structured data image :locale' => 'Afbeelding voor gestructureerde data :locale',
+    'logo structured data' => 'Afbeelding voor gestructureerde data',
+    'social media links' => 'Sociale media links',
+];
--- src/Settings/SocialMediaLinks.php
+++ src/Settings/SocialMediaLinks.php
@@ -4,18 +4,27 @@ namespace Codedor\SocialMediaLinks\Settings;
 
 use Codedor\FilamentSettings\Settings\SettingsInterface;
 use Filament\Forms\Components\TextInput;
+use Illuminate\Support\Str;
 
 class SocialMediaLinks implements SettingsInterface
 {
     public static function schema(): array
     {
         return collect(config('filament-social-media-links.platforms', []))
-            ->map(fn ($icon, $platform) => TextInput::make("filament-social-media-links.{$platform}"))
+            ->map(
+                fn ($icon, $platform) => TextInput::make("filament-social-media-links.{$platform}")
+                    ->label(Str::headline($platform))
+            )
             ->toArray();
     }
 
     public static function priority(): int
     {
         return 40;
+    }
+
+    public static function title(): string
+    {
+        return __('filament-social-media-links::settings.social media links');
     }
 }
